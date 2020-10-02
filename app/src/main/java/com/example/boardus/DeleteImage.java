be1425f5f40fd1ac56e6932791eb12f7f6e954a7
ashansameera <aweerasinghe40@gmail.com>
2020-10-02 18:28:13 +0530

    FeedBack,DeleteImage

--- app/src/main/java/com/example/boardus/DeleteImage.java
+++ app/src/main/java/com/example/boardus/DeleteImage.java
@@ -1,8 +1,8 @@
 package com.example.boardus;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class DeleteImage extends AppCompatActivity {
 
