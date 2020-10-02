da6dcd2378771be02948ee959dc929db30941f6b
ashansameera <aweerasinghe40@gmail.com>
2020-10-02 18:07:57 +0530

    FeedBack

--- app/src/main/java/com/example/boardus/FeedBack.java
+++ app/src/main/java/com/example/boardus/FeedBack.java
@@ -1,14 +1,14 @@
 package com.example.boardus;
 
-import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class FeedBack extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.FeedBack);
+        setContentView(R.layout.activity_feed_back);
     }
 }
